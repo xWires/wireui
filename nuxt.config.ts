82cdddc81a16599fd39dd697f0e9aecfdbe25b4d
xWires <xwires@tangledwires.xyz>
2025-05-30 19:19:15 +0100

    Set name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  $meta: {
+    name: "wireui"
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
 })
